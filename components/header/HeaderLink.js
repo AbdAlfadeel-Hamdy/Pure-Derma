edfb01d2cfb51157e141f833c255769635485d9a
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-05-03 18:08:51 +0300

    finish gallery

--- components/header/HeaderLink.js
+++ components/header/HeaderLink.js
@@ -4,7 +4,7 @@ const HeaderLink = ({ children, className }) => {
   return (
     <Link
       href="/cart"
-      className={`flex items-center justify-center hover:bg-gray-light-2 h-16 sm:h-20 px-2 basis-16 relative ${className}`}
+      className={`flex items-center justify-center hover:bg-gray-light-2 h-16 sm:h-20 px-2 sm:px-3 lg:px-4 basis-16 relative ${className}`}
     >
       {children}
     </Link>
--- components/header/HeaderNav.js
+++ components/header/HeaderNav.js
@@ -6,7 +6,7 @@ const HeaderNav = () => {
     <nav className="flex items-center basis-24">
       <HeaderLink>
         <IoCart className="w-5 sm:w-6 h-5 sm:h-6" />
-        <span className="bg-primary h-4 w-4 rounded-full absolute top-4 sm:top-6 right-0 text-white flex items-center justify-center font-thin text-xs">
+        <span className="bg-primary-dark h-4 w-4 rounded-full absolute top-4 sm:top-6 right-0 sm:right-1 lg:right-2 text-white flex items-center justify-center font-thin text-xs">
           1
         </span>
       </HeaderLink>
