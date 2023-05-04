42324234c7a1f366e25cbbaccd4de62ee0a5fc45
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-05-04 07:40:03 +0300

    finish overview

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="ar" dir="rtl">
       <Head />
-      <body className=" text-gray-dark-2 bg-gradient-to-br from-primary-light to-primary-dark bg-cover bg-no-repeat min-h-screen">
+      <body className=" text-gray-dark-2 leading-relaxed bg-gradient-to-br from-primary-light to-primary-dark bg-cover bg-no-repeat min-h-screen">
         <Main />
         <NextScript />
       </body>
