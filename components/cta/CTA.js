2951ae69b564aaf57774ceb310abfd7f55ceabb9
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-05-05 17:19:14 +0300

    update product item

--- components/cta/CTA.js
+++ components/cta/CTA.js
@@ -3,7 +3,7 @@ const { default: HeadingSecondary } = require("../ui/HeadingSecondary");
 
 const CTA = () => {
   return (
-    <section className="py-8">
+    <section className="py-8 px-4">
       <div className="px-2">
         <HeadingSecondary className="mb-8">انضم إلينا</HeadingSecondary>
         <SignupForm />
