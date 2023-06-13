ca9c10113b3df5c858e2a7b514e1a885fae84d7d
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-06-13 16:19:13 +0300

    add home images

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,9 @@ import imageThree from "@/public/images/home/3.png";
 import imageFour from "@/public/images/home/4.png";
 import imageFive from "@/public/images/home/5.png";
 import imageSix from "@/public/images/home/6.png";
+import facebookImage from "@/public/images/home/facebook.png";
+import tiktokImage from "@/public/images/home/tiktok.png";
+import instagramImage from "@/public/images/home/instagram.png";
 
 export default function Home({ products }) {
   return (
@@ -25,7 +28,44 @@ export default function Home({ products }) {
       <Image src={imageThree} />
       <Image src={imageFour} />
       <Image src={imageFive} />
-      <Image src={imageSix} />
+      <div className="relative">
+        <Image src={imageSix} />
+        <div
+          dir="ltr"
+          className="flex gap-1 sm:gap-2 md:gap-3 lg:gap-4 absolute top-3/4 left-[60%] lg:left-[65%] "
+        >
+          <a
+            href="https://web.facebook.com/puredermaeg
+"
+          >
+            <Image
+              src={facebookImage}
+              width={100}
+              height={100}
+              className="w-10 sm:w-16 md:w-20 lg:w-24"
+            />
+          </a>
+          <a
+            href="https://www.tiktok.com/@puredermaeg
+"
+          >
+            <Image
+              src={tiktokImage}
+              width={100}
+              height={100}
+              className="w-10 sm:w-16 md:w-20 lg:w-24"
+            />
+          </a>
+          <a href="https://www.instagram.com/puredermaeg/">
+            <Image
+              src={instagramImage}
+              width={100}
+              height={100}
+              className="w-10 sm:w-16 md:w-20 lg:w-24"
+            />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
